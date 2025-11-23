0c11d2f16b0045cc34b3cd31edc4f61cf731464d
unknown <iftekherakash6@gmail.com>
2025-11-24 02:05:30 +0600

    file structure

---
+++ src/app/About/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black" >page</div>
+  )
+}
